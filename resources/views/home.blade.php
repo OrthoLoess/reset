4d3cdd56bd26523349440c1df82e413df08628df
Ed Landamore <ortho@ratship.net>
2016-03-04 16:31:35 +0000

    Add view files

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<div class="jumbotron">
+    <h1>Reset App</h1>
+    <h3>for NPSI</h3>
+</div>
+<div class="row">
+    <div class="col-sm-5 col-sm-offset-1">
+        Button 1
+    </div>
+    <div class="col-sm-5">
+        Button 2
+    </div>
+</div>
